4fbf66ba6385d87a4c24dbe7889e923431765dd2
maheshikakodagoda <141060218+maheshikakodagoda@users.noreply.github.com>
2023-07-31 23:56:42 +0530

    Add files via upload

---
+++ lab1/Question2/Nd.java
@@ -0,0 +1,8 @@
+public class Nd {
+
+    public static void main(String[] args) {
+               System.out.println("Chamodi Maheshika");
+        System.out.println("Managment Informational system");
+
+    }
+}
---
+++ lab2/part1/monster.java
@@ -0,0 +1,10 @@
+
+    class monster extends item {
+        public class monster {
+    // Constructor
+    public monster(int location, String description) {
+        super(location, description);
+    }
+}
+
+}
